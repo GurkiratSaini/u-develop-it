7aa32ba0e64860feee20fd2ee36b1682ee2c016e
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-02 19:38:02 -0400

    added link to mysql database

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for nay other request (Not Found)\
 app.use((req, res) => {
